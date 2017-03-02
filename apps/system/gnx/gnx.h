3e83fd346f11a1b3f19aae5b0301b1f9d80939cf
Antonino Natale <antonio.natale97@hotmail.com>
2017-03-02 18:22:18 +0100

    Boh

--- apps/system/gnx/gnx.h
+++ apps/system/gnx/gnx.h
@@ -2,10 +2,21 @@
 #define _GNX_H
 
 #include <list>
+#include <aplus/gshm.h>
 
 using namespace std;
 
+
 namespace GNX {
+    
+    class Screen {
+        public:
+            static int Width;
+            static int Height;
+            static int Bpp;
+            static int Stride;
+            static void* FrameBuffer;
+    };
     
     class Rectangle {
         public:
@@ -13,13 +24,107 @@ namespace GNX {
             int Y;
             int Width;
             int Height;
+            
+            Rectangle(int x, int y, int w, int h)
+                : X(x), Y(y), Width(w), Height(h) { }
+            
+            inline int Right() {
+                return this->X + this->Width;
+            }
+            
+            inline int Bottom() {
+                return this->Y + this->Height;
+            }
+            
+            inline Rectangle* Clone() {
+                return new Rectangle(this->X, this->Y, this->Width, this->Height);
+            }
+            
+            
+            
+            inline list<Rectangle*>* Split(Rectangle* cr) {
+                list<Rectangle*>* ll = new list<Rectangle*>();
+                Rectangle* tmp = this->Clone();
+                
+                if(cr->X >= tmp->X && cr->X <= tmp->Right()) {
+                    ll->push_back(new Rectangle(tmp->X, tmp->Y, tmp->X - cr->X - 1, tmp->Height));
+                    tmp->X = cr->X;
+                }
+                
+                if(cr->Y >= tmp->Y && cr->Y <= tmp->Bottom()) {
+                    ll->push_back(new Rectangle(tmp->X, tmp->Y, tmp->Width, tmp->Y - cr->Y - 1));
+                    tmp->Y = cr->Y;
+                }
+                    
+                if(cr->Right() >= tmp->X && cr->Right() <= tmp->Right()) {
+                    ll->push_back(new Rectangle(cr->Right() + 1, tmp->Y, tmp->Width, tmp->Height));
+                    tmp->Width = cr->Width;
+                }
+
+                if(cr->Bottom() >= tmp->Y && cr->Bottom() <= tmp->Bottom()) {
+                    ll->push_back(new Rectangle(tmp->X, cr->Bottom() + 1, tmp->Width, tmp->Height));
+                    tmp->Height = cr->Height;
+                }
+                
+                delete tmp;
+                return ll;
+            }
+            
+            inline void AddClip(list<Rectangle*>* cl) {
+                for(int i = 0; i < cl->size();) {
+                    auto it = cl->begin();
+                    advance(it, i);
+                    
+                    
+                    if(
+                        (*it)->X <= this->Right()     &&
+                        (*it)->Right() >= this->X     &&
+                        (*it)->Y <= this->Bottom()    &&
+                        (*it)->Bottom() >= this->Y
+                    ) {
+                        i++;
+                        continue;
+                    }
+                    
+                    list<Rectangle*>* ll = (*it)->Split(this);
+                    cl->remove((*it));
+                    cl->merge(*ll);
+                    
+                    delete ll;
+                    i = 0;
+                }
+                
+                cl->push_back(this);
+            }
+            
+           inline void Draw(void (*plot) (int x, int y)) {
+                for(int x = 0; x < this->Width; x++) {
+                    plot(this->X + x, this->Y);
+                    plot(this->X + x, this->Y + this->Height);
+                }
+                
+                for(int y = 0; x < this->Height; y++) {
+                    plot(this->X, this->Y + y);
+                    plot(this->X + this->Width, this->Y + y);
+                }
+           }
     };
     
     class Surface {
         public:
-            Surface(uint16_t width, uint16_t height);
-            Surface(uint16_t width, uint16_t height, void* buffer);
-            ~Surface();
+            Surface(uint16_t width, uint16_t height)
+                : X(0), Y(0), Width(width), Height(height) {
+                    
+                this->Stride = width * (Screen::Bpp >> 3);
+                this->ClipRectangles = new list<Rectangle*>();
+                this->Buffer = (void*) gnew char[this->Stride * height];
+            }
+            
+            
+            ~Surface() {
+                delete this->ClipRectangles;
+                gfree(this->Buffer);
+            }
             
             uint16_t X;
             uint16_t Y;
@@ -27,17 +132,34 @@ namespace GNX {
             uint16_t Height;
             uint32_t Stride;
             void* Buffer;
+            
+            list<Rectangle*>* ClipRectangles;
     };
     
     class Window {
         public:
-            Window();
-            ~Window();
+            Window(Window* parent, string title, int w, int h) {
+                this->Title = title;
+                this->Context = new Surface(w, h);
+                this->Childs = new list<Window*> ();
+                this->Parent = parent;
+                
+                if(this->Parent)
+                    this->Parent->Childs->push_back(this);
+            }
+            
+            ~Window() {
+                delete this->Context;
+                delete this->Childs;
+                
+                if(this->Parent)
+                    this->Parent->Childs->remove(this);
+            }
             
             string Title;
-            Surface* surface;
+            Surface* Context;
             
-            list<Window*> Childs;
+            list<Window*>* Childs;
             Window* Parent;
     };
 }
--- apps/system/gnx/main.cpp
+++ apps/system/gnx/main.cpp
@@ -1,10 +1,58 @@
-#include <aplus/gshm.h>
 #include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <aplus/gshm.h>
+#include <aplus/fbdev.h>
+#include <aplus/sysconfig.h>
 #include "gnx.h"
+
+using namespace std;
+using namespace GNX;
+
 
 
 int main(int argc, char** argv) {
+    int fd = open("/dev/fb0", O_RDONLY);
+    if(fd < 0) {
+        cerr << "/dev/fb0: could not open framebuffer" << endl;
+        return -1;
+    }
     
     
+    fbdev_mode_t m;
+    m.width = (uint16_t) sysconfig("screen.width", SYSCONFIG_FORMAT_INT, Screen::Width);
+    m.height = (uint16_t) sysconfig("screen.height", SYSCONFIG_FORMAT_INT, Screen::Height);
+    m.bpp = (uint16_t) sysconfig("screen.bpp", SYSCONFIG_FORMAT_INT, Screen::Bpp);
+    m.vx =
+    m.vy = 0;
+    m.lfbptr = NULL;
+    
+    ioctl(fd, FBIOCTL_SETMODE, &m);
+    ioctl(fd, FBIOCTL_GETMODE, &m);
+    close(fd);
+    
+    
+    Screen::Width = m.width;
+    Screen::Height = m.height;
+    Screen::Bpp = m.bpp;
+    Screen::Stride = m.width * (m.bpp >> 3);
+    Screen::FrameBuffer = m.lfbptr;
+    
+    
+    Window* Desktop = new Window(NULL, "Desktop", Screen::Width, Screen::Height);
+    Window* Win1 = new Window(Desktop, "Win1", 600, 400);
+    Window* Win2 = new Window(Desktop, "Win2", 400, 300);
+    Window* Win3 = new Window(Desktop, "Win3", 400, 300);
+
+    
+    
+    //mx(Desktop);
+    mx(Win1);
+    mx(Win2);
+    mx(Win3);
+    
+  
+ 
     return 0;
 }
