c23c00a0ca90a2d9b2a9d8f25196de854448c0d5
Antonino Natale <antonio.natale97@hotmail.com>
2017-03-09 02:29:55 +0100

    Added Headers

--- include/aplus/base.h
+++ include/aplus/base.h
@@ -19,11 +19,11 @@
 
 
 #ifdef __GNUC__
-#define likely(x)						__builtin_expect(!!(x), 1)
-#define unlikely(x)						__builtin_expect(!!(x), 0)
+#define likely(x)                       __builtin_expect(!!(x), 1)
+#define unlikely(x)                     __builtin_expect(!!(x), 0)
 #else
-#define likely(x)						(!!(x))
-#define unlikely(x)						(!!(x))
+#define likely(x)                       (!!(x))
+#define unlikely(x)                     (!!(x))
 #endif
 
 
