b88c49ec9ab8f0f0ad3d0443be3a03a0a9c55735
Antonino Natale <antonio.natale97@hotmail.com>
2021-11-17 19:32:13 +0100

    Update License

--- include/arch/x86/cpu.h
+++ include/arch/x86/cpu.h
@@ -1,3 +1,26 @@
+/*                                                                      
+ * Author(s):                                                           
+ *      Antonino Natale <antonio.natale97@hotmail.com>                  
+ *                                                                      
+ * Copyright (c) 2013-2019 Antonino Natale                              
+ *                                                                      
+ *                                                                      
+ * This file is part of aplus.                                          
+ *                                                                      
+ * aplus is free software: you can redistribute it and/or modify        
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or    
+ * (at your option) any later version.                                  
+ *                                                                      
+ * aplus is distributed in the hope that it will be useful,             
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+ * GNU General Public License for more details.                         
+ *                                                                      
+ * You should have received a copy of the GNU General Public License    
+ * along with aplus.  If not, see <http://www.gnu.org/licenses/>.       
+ */                                                                     
+                                                                        
 #ifndef _ARCH_X86_CPU_H
 #define _ARCH_X86_CPU_H
 
@@ -633,7 +656,8 @@ static inline unsigned long long x86_rdtsc(void) {
     unsigned long long vh;
 
     __asm__ __volatile__ (
-        "lfence; rdtsc; lfence;\n" 
+        "lfence; rdtsc; lfence;
+" 
         : "=a"(vl), "=d"(vh)
     );
 
--- include/arch/x86/smp.h
+++ include/arch/x86/smp.h
@@ -1,3 +1,26 @@
+/*                                                                      
+ * Author(s):                                                           
+ *      Antonino Natale <antonio.natale97@hotmail.com>                  
+ *                                                                      
+ * Copyright (c) 2013-2019 Antonino Natale                              
+ *                                                                      
+ *                                                                      
+ * This file is part of aplus.                                          
+ *                                                                      
+ * aplus is free software: you can redistribute it and/or modify        
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or    
+ * (at your option) any later version.                                  
+ *                                                                      
+ * aplus is distributed in the hope that it will be useful,             
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+ * GNU General Public License for more details.                         
+ *                                                                      
+ * You should have received a copy of the GNU General Public License    
+ * along with aplus.  If not, see <http://www.gnu.org/licenses/>.       
+ */                                                                     
+                                                                        
 #ifndef _APLUS_X86_SMP_H
 #define _APLUS_X86_SMP_H
 
