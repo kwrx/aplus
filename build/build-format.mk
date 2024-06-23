90f360a243cf0237b658e4a9506064227810bcac
Antonino Natale <antonio.natale97@hotmail.com>
2024-06-23 19:23:07 +0200

    build: update build-format.mk to include additional directories

--- build/build-format.mk
+++ build/build-format.mk
@@ -1,8 +1,10 @@
 .PHONY: format
 .NOTPARALLEL: format
 
+DIRS := $(PROJECTS) lib libk arch include
+
 format:
-	$(QUIET)echo "    FMT     $(PROJECTS)"
-	$(QUIET)find $(PROJECTS) -name "*.[ch]" -exec echo "    FMT     {}" \; -exec clang-format-18 -i {} \;
+	$(QUIET)echo "    FMT     $(DIRS)"
+	$(QUIET)find $(DIRS) -name "*.[ch]" -exec echo "    FMT     {}" \; -exec clang-format-18 -i {} \;
 
 
