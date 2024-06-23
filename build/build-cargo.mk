287371ff7552563daf3099ed837dd30445288516
Antonino Natale <antonio.natale97@hotmail.com>
2024-06-23 19:12:18 +0200

    refactor: update Makefile to include kernel-rust in the build process

--- build/build-cargo.mk
+++ build/build-cargo.mk
@@ -12,7 +12,7 @@ include $(ROOTDIR)/build/build-flags.mk
 $(TARGET): Cargo.toml
 	$(QUIET)echo "    CARGO   $(shell realpath --relative-base=$(ROOTDIR) $@)"
 	$(QUIET)$(CARGO) build $(CARGO_FLAGS)
-	$(QUIET)$(CP) target/$(CARGO_TARGET)/$(CARGO_BUILD)/$(notdir $(TARGET)) $@
-	$(QUIET)$(CP) target/$(CARGO_TARGET)/$(CARGO_BUILD)/$(notdir $(shell basename $(TARGET) .a)).rlib $(shell basename $@ .a).rlib
+	$(QUIET)$(CP) target/$(CARGO_TARGET)/$(CARGO_BUILD)/$(notdir $(shell echo $(TARGET) | sed 's/\-/_/g')) $@
+	$(QUIET)$(CP) target/$(CARGO_TARGET)/$(CARGO_BUILD)/$(notdir $(shell basename $(shell echo $(TARGET) | sed 's/\-/_/g') .a)).rlib $(shell basename $@ .a).rlib
 
 include $(ROOTDIR)/build/build-objects.mk
