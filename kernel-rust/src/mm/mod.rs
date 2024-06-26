ab7c61bfad0458c7c654d6b9d8d11ea6ef66f8cf
Antonino Natale <antonio.natale97@hotmail.com>
2024-06-27 00:09:39 +0200

    feat(kernel-rust): rewrite kernel heap in rust

--- kernel-rust/src/mm/mod.rs
+++ kernel-rust/src/mm/mod.rs
@@ -23,32 +23,4 @@
  * along with aplus.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// #[import_c_var(GFP_KERNEL)]
-
-use crate::bindings::GFP_KERNEL;
-use crate::mm::heap::{kcalloc, kfree, kmalloc};
-use core::alloc::{GlobalAlloc, Layout};
-
-pub struct Allocator;
-
-unsafe impl GlobalAlloc for Allocator {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        kmalloc(layout.size(), GFP_KERNEL)
-    }
-
-    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        kcalloc(1, layout.size(), GFP_KERNEL)
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        kfree(ptr);
-    }
-}
-
-#[global_allocator]
-static ALLOCATOR: Allocator = Allocator;
-
-#[alloc_error_handler]
-fn alloc_error_handler(layout: Layout) -> ! {
-    panic!("allocation error: {:?}", layout)
-}
+pub mod heap;
